567e9dcae912672d968bb3a950152b2286c03bf7
tofubridge <jqiao9011@gmail.com>
2018-08-08 15:00:08 -0400

    Add main .py file

---
+++ colors_gui.py
@@ -0,0 +1,17 @@
+# =====================
+# import libraries
+# =====================
+import tkinter as tk
+from tkinter import filedialog
+from PIL import Image, ImageTk
+
+# =====================
+
+root = tk.Tk()
+root.title("Color!!")
+
+#pil_image = Image.open(filedialog.askopenfile())
+
+#img = ImageTk.PhotoImage(pil_image)
+
+root.mainloop()
